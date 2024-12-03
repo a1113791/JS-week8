a72712784b2827389e060dd9264ec430f03689c4
hua <a1113791@gmail.com>
2024-12-04 01:21:29 +0800

    前台購物車處理

--- all.js
+++ all.js
@@ -11,12 +11,11 @@ function getProduct() {
   axios
     .get(`${customerApi}/products`)
     .then((res) => {
-      //   console.log(res.data.products);
       productData = res.data.products;
       renderProduct(productData);
     })
     .catch((err) => {
-      console.log(err);
+      alert(err);
     });
 }
 
@@ -31,7 +30,7 @@ function renderProduct(data) {
 					<img
 						src="${item.images}"
 						alt="" />
-					<a href="#" class="addCardBtn">加入購物車</a>
+					<a href="#" class="addCardBtn" data-id=${item.id}>加入購物車</a>
 					<h3>${item.title}</h3>
 					<del class="originPrice">NT$${item.origin_price}</del>
 					<p class="nowPrice">NT$${item.price}</p>
@@ -55,12 +54,104 @@ function filterProduct(value) {
   renderProduct(result);
 }
 productSelect.addEventListener("change", (e) => {
-  //   console.log(e.target.value);
   filterProduct(e.target.value);
 });
+
+//新增購物車
+function addCart(id) {
+  const data = {
+    data: {
+      productId: id,
+      quantity: 1,
+    },
+  };
+  axios
+    .post(`${customerApi}/carts`, data)
+    .then((res) => {
+      cartData = res.data.carts;
+      renderCart();
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
+productWrap.addEventListener("click", (e) => {
+  e.preventDefault();
+  addCart(e.target.dataset.id);
+});
+
+//刪除購物車所有商品
+const discardAllBtn = document.querySelector(".discardAllBtn");
+
+function deleteAllCart() {
+  axios
+    .delete(`${customerApi}/carts`)
+    .then((res) => {
+      cartData = res.data.carts;
+      renderCart();
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
+discardAllBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  deleteAllCart();
+});
+
+//渲染購物車
+function getCart() {
+  axios
+    .get(`${customerApi}/carts`)
+    .then((res) => {
+      cartData = res.data.carts;
+      renderCart();
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
+const shoppingCartTableBody = document.querySelector(
+  ".shoppingCart-table tbody"
+);
+const shoppingCartTableFoot = document.querySelector(
+  ".shoppingCart-table tfoot"
+);
+
+function renderCart() {
+  if (cartData.length === 0) {
+    shoppingCartTableBody.innerHTML = "購物車目前無商品";
+    shoppingCartTableFoot.innerHTML = "";
+    return;
+  }
+  let str = "";
+  cartData.forEach((item) => {
+    str += `<tr>
+							<td>
+								<div class="cardItem-title">
+									<img
+										src="${item.product.images}"
+										alt="" />
+									<p>${item.product.title}</p>
+								</div>
+							</td>
+							<td>NT$${item.product.origin_price}</td>
+							<td>${item.quantity}</td>
+							<td>NT$${item.product.price}</td>
+							<td class="discardBtn">
+								<a href="#" class="material-icons"> clear </a>
+							</td>
+						</tr>`;
+  });
+  shoppingCartTableBody.innerHTML = str;
+}
 
 //初始化
 function init() {
   getProduct();
+  getCart();
 }
 init();
